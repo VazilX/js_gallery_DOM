196865bddf2b23b98d60dfc64c45f8ab7cfb9bdc
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-19 10:24:22 +0200

    make build

---
+++ index.f1cc3740.js
@@ -0,0 +1,2 @@
+document.querySelector("ul").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");t&&(document.querySelector(".gallery__large-img").src=t.href)});
+//# sourceMappingURL=index.f1cc3740.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const list = document.querySelector("ul");
+list.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const linkElement = e.target.closest(".list-item__link");
+    if (!linkElement) // eslint-disable-next-line
+    return;
+    const largeImg = document.querySelector(".gallery__large-img");
+    largeImg.src = linkElement.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
